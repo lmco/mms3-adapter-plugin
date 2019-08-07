a3ef5ab31cdd3e0c34f20da393eb54b679fe9d59
Bieber, Austin J <austin.j.bieber@lmco.com>
2019-08-07 14:15:52 -0600

    Handle login

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express();
+const auth = M.require('lib.auth');
+const APIController = M.require('controllers.api-controller');
+
+
+app.route('/login')
+.post(
+	auth.authenticate,
+	auth.doLogin,
+	(req, res, next) => {
+		console.log(`${req.method}: ${req.originalUrl}`);
+		addHeaders(req, res);
+		return res.status(200).send(req.session.token);
+	}
+)
+.options(
+	(req, res, next) => {
+		console.log(`${req.method}: ${req.originalUrl}`);
+		addHeaders(req, res);
+		return res.sendStatus(200);
+	}
+);
 
 app.use('*', (req, res, next) => {
-	console.log(`${req.method}: ${req.baseUrl}`);
+	console.log(`${req.method}: ${req.originalUrl}`);
 	return res.status(501).send('Not Implemented');
 });
+
+function addHeaders(req, res) {
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Headers', 'Content-Type')
+}
 
 module.exports = app;
