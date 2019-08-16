d659c4e3be180c8f929902631b018302da7d9811
Leah De Laurell <leah.p.delaurell@lmco.com>
2019-08-16 14:40:21 -0600

    updated

---
+++ reformat-controller/format-element.js
@@ -0,0 +1,19 @@
+const utils = M.require('lib.utils');
+
+// Export the module
+module.exports = function formatElement(elemObj) {
+	return {
+		id: utils.parseID(elemObj._id).pop(),
+		documentation: elemObj.documentation,
+		type: elemObj.type,
+		ownerId: () ? utils.parseID(elemObj.parent).pop() : '',
+		name: elemObj.name,
+		_projectId: utils.parseID(elemObj.project).pop(),
+		_refId: utils.parseID(elemObj.branch).pop(),
+		_creator: elemObj.createdBy,
+		_created: elemObj.createdOn,
+		_modifier: elemObj.lastModifiedBy,
+		_modified: elemObj.updatedOn,
+		_editable: true
+	};
+}
