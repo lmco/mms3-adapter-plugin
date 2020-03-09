71fef7b26bf492e87546d8e5d9f5aef1ce54e6f0
Connor Doyle <connor.p.doyle@lmco.com>
2020-03-09 17:01:52 -0600

    projectID bug

--- src/api-controller.js
+++ src/api-controller.js
@@ -236,7 +236,7 @@ async function postProjects(req, res, next) {
     await utils.asyncForEach(projects, async (project) => {
       const projectID = mcfUtils.parseID(project._id).pop();
       const elem = {
-        id: `view_instances_bin${projectid}`,
+        id: `view_instances_bin${projectID}`,
         name: 'View Instances Bin',
         parent: 'model'
       };
